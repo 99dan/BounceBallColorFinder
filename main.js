0c18d0edec838b5e8af7d64bd73eb5f4d69211b9
99dan <61859113+99dan@users.noreply.github.com>
2024-06-02 19:22:36 +0900

    create select image and ux

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
+const ctx=canvas.getContext('2d');
+
 const canvasDiv=document.getElementsByClassName('canvas-div')[0];
 
 const colorPicker=document.getElementById('color-picker');
 const colorList=document.getElementById('color-list');
 const colorPickerLabel=document.getElementById('color-picker-label');
+const fileElement=document.getElementById('file--element');
+
+let selectedImage=null;
+
+const backgroundBitmap=(function()
+{
+  let size=20;
+  const canvas=new OffscreenCanvas(size,size);
+  const ctx=canvas.getContext('2d');
+  ctx.fillStyle='#fff';
+  ctx.fillRect(0,0,size,size);
+  ctx.fillStyle='#ccc';
+  ctx.fillRect(0,0,size/2,size/2);
+  ctx.fillRect(size/2,size/2,size/2,size/2);
+  return canvas;
+})();
+
+let camera=
+{
+    x:0,y:0,zoom:1
+};
+
+function drawBackground(ctx)
+{
+    for(let x=0;x<canvas.width;x+=backgroundBitmap.width)
+        for(let y=0;y<canvas.height;y+=backgroundBitmap.height)
+            ctx.drawImage(backgroundBitmap,x,y);
+}
 
 function onWindowResize()
 {
@@ -27,82 +57,140 @@ function draw()
 
 function realDraw()
 {
+    drawBackground(ctx);
 
+    ctx.translate(-camera.x,-camera.y);
+    ctx.scale(camera.zoom,camera.zoom);
+
+    ctx.imageSmoothingEnabled=false;
+    if(selectedImage!==null)
+        ctx.drawImage(selectedImage,0,0);
+    ctx.imageSmoothingEnabled=true;
+
+    ctx.resetTransform();
 }
+
+function isInCanvas(x,y)
+{
+    let b=canvas.getBoundingClientRect();
+    return b.left<=x&&x<=b.right&&b.top<=y&&y<=b.bottom;
+}
+
+let pointers={},lastX=0,lastY=0,zoomPointersIds=null,lastZoomPos=null;
+window.onpointerdown=function(e)
+{
+    if(e.isPrimary)
+    {
+        lastX=e.clientX;
+        lastY=e.clientY;
+    }
+    let x=e.clientX,y=e.clientY;
+    if(!isInCanvas(x,y))return;
+
+    let ids=Object.keys(pointers);if(ids.length>=2)zoomPointersIds=ids.slice(0,2);else zoomPointersIds=null;
+    
+    pointers[e.pointerId]=e;
+};
+window.onpointermove=function(e)
+{
+    if(e.isPrimary)
+    {
+        lastX=e.clientX;
+        lastY=e.clientY;
+    }
+    let pe=pointers[e.pointerId];
+    pointers[e.pointerId]=e;
+    if(e.pressure<=0)return;
+
+    let ids=Object.keys(pointers);if(ids.length>=2)zoomPointersIds=ids.slice(0,2);else zoomPointersIds=null;
+
+    if(zoomPointersIds!==null)
+    {
+        let p1=pointers[zoomPointersIds[0]],p2=pointers[zoomPointersIds[1]];
+        let p1x=p1.clientX,p1y=p1.clientY,p2x=p2.clientX,p2y=p2.clientY;
+
+        if(lastZoomPos!==null)
+        {
+            let [[pp1x,pp1y],[pp2x,pp2y]]=lastZoomPos;
+
+            let cx=(p1x+p2x)/2,cy=(p1y+p2y)/2;
+            let pcx=(pp1x+pp2x)/2,pcy=(pp1y+pp2y)/2;
+
+            let zp=((p1x-p2x)**2+(p1y-p2y)**2)**.5/((pp1x-pp2x)**2+(pp1y-pp2y)**2)**.5
+
+            let newZoom=camera.zoom*zp;
+
+            let cmx=(e.clientX+camera.x)/camera.zoom;
+            let cmy=(e.clientY+camera.y)/camera.zoom;
+
+            let ncx=(cmx*newZoom)-e.clientX;
+            let ncy=(cmy*newZoom)-e.clientY;
+
+            camera.x=ncx;
+            camera.y=ncy;
+
+            camera.zoom=newZoom;
+
+
+            camera.x-=cx-pcx;
+            camera.y-=cy-pcy;
+        }
+        lastZoomPos=[[p1x,p1y],[p2x,p2y]];
+    }
+    else
+    {
+        camera.x-=e.clientX-pe.clientX;
+        camera.y-=e.clientY-pe.clientY;
+    }
+    
+    draw();
+};
+window.onpointerup=function(e)
+{
+    if(e.isPrimary)
+    {
+        lastX=e.clientX;
+        lastY=e.clientY;
+    }
+    delete pointers[e.pointerId];
+
+    let ids=Object.keys(pointers);if(ids.length>=2)zoomPointersIds=ids.slice(0,2);else zoomPointersIds=null;
+};
+window.onwheel=function(e)
+{
+    let pe=null;
+    for(let id in pointers)
+    {
+        if(pointers[id].isPrimary)
+        {
+            pe=pointers[id];
+            break;
+        }
+    }
+    if(pe===null)return;
+
+    let dir=-Math.sign(e.deltaY);
+    let newZoom=camera.zoom*1.5**dir;
+
+    let cx=(e.clientX+camera.x)/camera.zoom;
+    let cy=(e.clientY+camera.y)/camera.zoom;
+
+    let ncx=(cx*newZoom)-e.clientX;
+    let ncy=(cy*newZoom)-e.clientY;
+
+    camera.x=ncx;
+    camera.y=ncy;
+
+    camera.zoom=newZoom;
+
+    draw();
+}
+
 
 function drawAnimationFrame()
 {
     realDraw();
     drawWait=false;
-}
-
-function arr2hex(arr)
-{
-    return arr.map(x=>Math.min(Math.max(Math.round(x),0),255).toString(16).padStart(2,'0')).join('')
-}
-
-function hex2arr(hex)
-{
-    return hex.split(/(..)/).filter((_,i)=>i%2).map(x=>parseInt(x,16));
-}
-
-function hexDarker(hex,n=0.9)
-{
-    return arr2hex(hex2arr(hex).map(x=>x*n));
-}
-
-function arrDarker(arr,n=0.9)
-{
-    return arr.map(x=>x*n);
-}
-
-function hexLighter(hex,n=0.9)
-{
-    return arr2hex(hex2arr(hex).map(x=>255-(255-x)*n));
-}
-
-function arrLighter(arr,n=0.9)
-{
-    return arr.map(x=>255-(255-x)*n);
-}
-
-function hexAdder(hex,n)
-{
-    return arr2hex(hex2arr(hex).map(x=>x+n));
-}
-
-function arrAdder(arr,n)
-{
-    return arr.map(x=>x+n);
-}
-
-function calcY(arr)
-{
-    let a=arr.map(x=>(x=Math.min(Math.max(x/255,0),1),x<=0.03928?x/12.92:((x+0.055)/1.055)**2.4));
-    let r=a[0],g=a[1],b=a[2];
-    return 0.299*r+0.587*g+0.114*b;
-}
-
-function calcL(a1,a2)
-{
-    a1=calcY(a1);
-    a2=calcY(a2);
-    let n=Math.min(a1,a2),m=Math.max(a1,a2);
-    return (m+0.05)/(n+0.05);
-}
-
-function hexDis(h1,h2)
-{
-    let h=hex2arr(h1),k=hex2arr(h2);
-    return h.reduce((a,c,i)=>a+(c-k[i])**2,0);
-}
-
-function hexVi(hex,n=128,n2=0.6)
-{
-    let arr=hex2arr(hex);
-    let cl=[arrAdder(arr,n),arrAdder(arr,-n),arrDarker(arr,n2),arrLighter(arr,n2)];
-    cl.sort((a,b)=>calcL(b,arr)-calcL(a,arr));
-    return arr2hex(cl[0]);
 }
 
 function colorListUpdate(list)
@@ -149,3 +237,33 @@ colorPicker.oninput=function()
 }
 
 window.onresize=onWindowResize;
+
+fileElement.onchange=()=>
+{
+    if(fileElement.files.length==0)return;
+
+    selectedImage=new Image();
+    var r=new FileReader();
+    r.onload=()=>
+    {
+        selectedImage.onload=imageOnLoad;
+        selectedImage.src=r.result;
+    };
+    r.readAsDataURL(fileElement.files[0]);
+};
+
+function imageOnLoad()
+{
+    document.getElementsByClassName('first-screen')[0].style.display='none';
+
+    camera.zoom=Math.min(canvas.width/selectedImage.width,canvas.height/selectedImage.height)*0.8;
+    camera.x=(selectedImage.width*camera.zoom-canvas.width)/2;
+    camera.y=(selectedImage.height*camera.zoom-canvas.height)/2;
+
+    draw();
+}
+
+window.onload=function()
+{
+    onWindowResize();
+};
--- util.js
+++ util.js
@@ -17,3 +17,72 @@ function findNearN(v,n)
         return[c,p];
     });
 }
+
+function arr2hex(arr)
+{
+    return arr.map(x=>Math.min(Math.max(Math.round(x),0),255).toString(16).padStart(2,'0')).join('')
+}
+
+function hex2arr(hex)
+{
+    return hex.split(/(..)/).filter((_,i)=>i%2).map(x=>parseInt(x,16));
+}
+
+function hexDarker(hex,n=0.9)
+{
+    return arr2hex(hex2arr(hex).map(x=>x*n));
+}
+
+function arrDarker(arr,n=0.9)
+{
+    return arr.map(x=>x*n);
+}
+
+function hexLighter(hex,n=0.9)
+{
+    return arr2hex(hex2arr(hex).map(x=>255-(255-x)*n));
+}
+
+function arrLighter(arr,n=0.9)
+{
+    return arr.map(x=>255-(255-x)*n);
+}
+
+function hexAdder(hex,n)
+{
+    return arr2hex(hex2arr(hex).map(x=>x+n));
+}
+
+function arrAdder(arr,n)
+{
+    return arr.map(x=>x+n);
+}
+
+function calcY(arr)
+{
+    let a=arr.map(x=>(x=Math.min(Math.max(x/255,0),1),x<=0.03928?x/12.92:((x+0.055)/1.055)**2.4));
+    let r=a[0],g=a[1],b=a[2];
+    return 0.299*r+0.587*g+0.114*b;
+}
+
+function calcL(a1,a2)
+{
+    a1=calcY(a1);
+    a2=calcY(a2);
+    let n=Math.min(a1,a2),m=Math.max(a1,a2);
+    return (m+0.05)/(n+0.05);
+}
+
+function hexDis(h1,h2)
+{
+    let h=hex2arr(h1),k=hex2arr(h2);
+    return h.reduce((a,c,i)=>a+(c-k[i])**2,0);
+}
+
+function hexVi(hex,n=128,n2=0.6)
+{
+    let arr=hex2arr(hex);
+    let cl=[arrAdder(arr,n),arrAdder(arr,-n),arrDarker(arr,n2),arrLighter(arr,n2)];
+    cl.sort((a,b)=>calcL(b,arr)-calcL(a,arr));
+    return arr2hex(cl[0]);
+}
